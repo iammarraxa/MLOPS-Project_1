26580c0a1606c86ca196bf2479689b50eb35ee42
Muhammad Ammar Raza <ammar.raza32002@gmail.com>
2025-10-11 22:04:43 +0500

    data transformation added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -3,16 +3,20 @@ from src.exception import MyException
 from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
 
 from src.entity.config_entity import (DataIngestionConfig,
-                                      DataValidationConfig)
+                                      DataValidationConfig,
+                                      DataTransformationConfig)
 from src.entity.artifact_entity import(DataIngestionArtifact,
-                                       DataValidationArtifact)
+                                       DataValidationArtifact,
+                                       DataTransformationArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     def start_data_ingestion(self):
 
@@ -40,13 +44,22 @@ class TrainPipeline:
             return data_validation_artifact
         except Exception as e:
             raise MyException(e, sys) from e
+        
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
 
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact, data_validation_artifact, self.data_transformation_config)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise MyException(e, sys) from e
 
     def run_pipeline(self) -> None:
 
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact, data_validation_artifact)
+
         except Exception as e:
             raise MyException(e, sys)
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -38,7 +38,7 @@ def save_object(file_path: str, obj: object) -> None:
     try:
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_path)
+            dill.dump(obj, file_obj)
         logging.info('Exited save_object mthod of utils')
     except Exception as e:
         raise MyException(e, sys) from e
@@ -47,7 +47,7 @@ def save_np_array_data(file_path: str, array: np.array):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, 'rb') as file_obj:
+        with open(file_path, 'wb') as file_obj:
             np.save(file_obj, array)
     except Exception as e:
         raise MyException(e, sys) from e
