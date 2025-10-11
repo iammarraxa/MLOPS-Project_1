c6a6a3503c6703a4ba311f5d7f8eed993daca1a9
Muhammad Ammar Raza <ammar.raza32002@gmail.com>
2025-10-11 19:53:02 +0500

    data validation added

---
+++ src/components/data_validation.py
@@ -0,0 +1,126 @@
+import json
+import os
+import sys
+import pandas as pd
+from pandas import DataFrame
+from src.exception import MyException
+from src.logger import logging
+from src.utils.main_utils import read_yaml_file
+from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from src.entity.config_entity import DataValidationConfig
+from src.constants import SCHEMA_FILE_PATH
+
+class DataValidation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_config: DataValidationConfig):
+
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise MyException(e, sys)
+        
+    def validate_num_of_columns(self, dataframe: DataFrame) -> bool:
+        '''
+        Output: bool vals based on validation results
+        '''
+        try:
+            status = len(dataframe.columns) == len(self._schema_config['columns'])
+            logging.info(f'is required column pesent: [{status}]')
+            return status
+        except Exception as e:
+            raise MyException(e, sys)
+        
+    def is_column_exist(self, dataframe: DataFrame) -> bool:
+        '''
+        validates existence of numeriacal & categorical colums
+
+        Output: bool vals based on validation results
+        '''
+        try:
+            dataframe_columns = dataframe.columns
+            missing_numerical_columns = []
+            missing_categorical_columns = []
+            for column in self._schema_config['numerical_columns']:
+                if column not in dataframe_columns:
+                    missing_numerical_columns.append(column)
+            
+            if len(missing_numerical_columns) > 0:
+                logging.info(f'Missing numerical colums: {missing_numerical_columns}')
+            
+            for column in self._schema_config['categorical_columns']:
+                if column not in dataframe_columns:
+                    missing_categorical_columns.append(column)
+            
+            if len(missing_categorical_columns) > 0:
+                logging.info(f'Missing categorical colums: {missing_categorical_columns}')
+
+            return False if len(missing_numerical_columns) > 0 or len(missing_categorical_columns) > 0 else True
+        except Exception as e:
+            raise MyException(e, sys)
+        
+    @staticmethod
+    def read_data(file_path) -> DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise MyException(e, sys)
+        
+    def initiate_data_validation(self) -> DataValidationArtifact:
+        '''
+        Output: bool vals based on validation results
+        '''
+        try:
+            validation_error_msg = ''
+            logging.info('Starting Data Validation')
+            train_df, test_df = (DataValidation.read_data(file_path=self.data_ingestion_artifact.trained_file_path),
+                                 DataValidation.read_data(file_path=self.data_ingestion_artifact.test_file_path))
+            
+            status = self.validate_num_of_columns(train_df)
+            if not status:
+                validation_error_msg += 'columns are missing in training datatframe'
+            else:
+                logging.info(f'All required columns present in training dataframe: {status}')
+
+            status = self.validate_num_of_columns(test_df)
+            if not status:
+                validation_error_msg += 'columns are missing in testing datatframe'
+            else:
+                logging.info(f'All required columns present in testing dataframe: {status}')
+
+            status = self.is_column_exist(train_df)
+            if not status:
+                validation_error_msg += 'columns are having datatype mismatch in training datatframe'
+            else:
+                logging.info(f'All required columns have correct datatypes in training dataframe: {status}')
+
+            status = self.is_column_exist(test_df)
+            if not status:
+                validation_error_msg += 'columns are having datatype mismatch in testing datatframe'
+            else:
+                logging.info(f'All required columns have correct datatypes in testing dataframe: {status}')
+
+            validation_status = len(validation_error_msg) == 0
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status= validation_status,
+                message= validation_error_msg,
+                validation_report_file_path=self.data_validation_config.validation_report_file_path
+            )
+
+            report_dir = os.path.dirname(self.data_validation_config.validation_report_file_path)
+            os.makedirs(report_dir, exist_ok=True)
+
+            validation_report = {
+                'validation_status': validation_status,
+                'message': validation_error_msg.strip()
+            }
+
+            with open(self.data_validation_config.validation_report_file_path, 'w') as report_file:
+                json.dump(validation_report, report_file, indent=4)
+
+            logging.info('Data validation artifact created & saved to json file')
+            logging.info(f'Data validation artifact: {data_validation_artifact}')
+            return data_validation_artifact
+        except Exception as e:
+            raise MyException(e, sys) from e
